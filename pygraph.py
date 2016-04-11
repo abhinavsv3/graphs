e001b2f2a9ac6513b9825f0a375347eb6e454f2e
Abhinav <abhinavsv3@gmail.com>
2016-04-11 16:00:15 +0200

    Final graph python Code

--- pygraph.py
+++ pygraph.py
@@ -1,20 +1,10 @@
 """
-File: graph.cpp
-// -- simple graph handling source file
-//-----------------------------------------------------------------------------
-// Community detection
-// Based on the article "Fast unfolding of community hierarchies in large networks"
-// Copyright (C) 2008 V. Blondel, J.-L. Guillaume, R. Lambiotte, E. Lefebvre
-//
-// This program must not be distributed without agreement of the above mentionned authors.
-//-----------------------------------------------------------------------------
-// Author   : E. Lefebvre, adapted by J.-L. Guillaume
-// Email    : jean-loup.guillaume@lip6.fr
-// Location : Paris, France
-// Time	    : February 2008
-//-----------------------------------------------------------------------------
-// see readme.txt for more details
-
+File: pygraph.py
+ -- simple graph handling source file
+Author   : Abhinav S V
+Email    : abhinavsv3@gmail.com
+Location : Barcelona, Spain
+Time	    : April 2016
 """
 class Graph:
 	"""gwt : graph's total weight
