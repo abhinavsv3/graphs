f87a79f82aa6241d2382d6d16e993d975501e1bb
Abhinav <abhinavsv3@gmail.com>
2016-04-11 15:56:16 +0200

    Final graph python Code

--- pygraph.py
+++ pygraph.py
@@ -1,3 +1,21 @@
+"""
+File: graph.cpp
+// -- simple graph handling source file
+//-----------------------------------------------------------------------------
+// Community detection
+// Based on the article "Fast unfolding of community hierarchies in large networks"
+// Copyright (C) 2008 V. Blondel, J.-L. Guillaume, R. Lambiotte, E. Lefebvre
+//
+// This program must not be distributed without agreement of the above mentionned authors.
+//-----------------------------------------------------------------------------
+// Author   : E. Lefebvre, adapted by J.-L. Guillaume
+// Email    : jean-loup.guillaume@lip6.fr
+// Location : Paris, France
+// Time	    : February 2008
+//-----------------------------------------------------------------------------
+// see readme.txt for more details
+
+"""
 class Graph:
 	"""gwt : graph's total weight
 	m: graph matrix in start,weight, destination
@@ -8,7 +26,7 @@ class Graph:
 	nodes : Set of all the nodes
 	lnodes: List of all nodes
 	type: 0 for not weighted (Default)
-	  	1 for weighted
+	  	  1 for weighted
 	"""
 	def __init__(self):
 		"""Creates an Empty, Unweighted Graph"""
@@ -45,7 +63,24 @@ class Graph:
 		self.nnodes = len(self.lnodes)
 		self.name = name
 		self.nedges = len(self.m)
-		print "Graph Created"
+#		print "Graph Created"
+
+	def createGraph(self, file, name = "Unnamed"):
+		"""Used to create a weighed graph given the file name of the stored graph
+		and also the name of the graph as an optinal parameter
+		"""
+		self.type = 0 # """UnWeighted Graph"""
+		with open(file) as f:
+			for line in f:
+				self.m.append(map(int, line.split()))
+		self.mt = [[row[i] for row in self.m] for i in range(2)]#"""Matrix Transpose"""		
+		self.gwt = 0
+		self.nodes= set(self.mt[0]).union(self.mt[1])
+		self.lnodes = list(self.nodes)
+		self.nnodes = len(self.lnodes)
+		self.name = name
+		self.nedges = len(self.m)
+#		print "Unweighted Graph Created"
 
 	def __repr__(self):
 		"""Displays the meta data of the graph"""
@@ -68,10 +103,34 @@ class Graph:
 	def __len__(self):
 		"""Number of nodes will be returned"""
 		return self.nnodes
+	def clean(self): #Same as reinitiaziling
+		"""Clean the graph and reinitialze a Null graph
+		"""
+		self.type=0
+		self.tname = ["Unweighted","Weighted"]
+		#"""Matrix"""
+		self.m = []
+		#"""Matrix Transpose"""
+		self.mt = []
+		#Set of all the nodes
+		self.nodes = set()
+		#List of all the nodes
+		self.lnodes = []
+		#Number of nodes
+		self.nnodes = 0
+		#Name
+		self.name = "Unnamed"
+		#Number of Edges
+		self.nedges = 0
 
+
+#Driver Tests
 filename = raw_input("Enter the Name of the File : ")
 g  =  Graph()
-g.createWeightedGraph(filename,"Karate")
+g.createGraph(filename,"Karate")
 print g
 help(g)
+print len(g)
+g.clean()
+print g
 print len(g)
