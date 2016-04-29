d5605ec3d5fae29b02026cd5b1b88e96fe8b9d24
Abhinav <abhinavsv3@gmail.com>
2016-04-29 18:57:18 +0200

    Test11

--- spliterinpy.py
+++ spliterinpy.py
@@ -1,6 +1,6 @@
 import louvaininpy as l
 import json
-import jsoninpy
+import jsoninpy as jj
 from copy import deepcopy
 
 g = l.louvaininpy()
@@ -14,4 +14,21 @@ else:
 partition, q= g.louvain()
 print partition
 roots = zip([i for i in range(len(partition))],[len(i) for i in partition])
-print roots
+y = 0
+d = {}
+for i in partition:
+	for j in i:
+		d[j] = y
+	y = y + 1
+print d
+
+print roots[0][0]
+
+#Create the root json
+newj = jj.jsoninpy()
+for i in roots:
+	newj.jsonRoot(i[0],i[1])
+newj.jsonDump("roots.json")
+
+
+print g.edges
